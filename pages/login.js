0cb8673d6e65b826fff3d9cc2dbbbfdb1cc3323a
batoo20005-dev <batoo20005@gmail.com>
2026-01-07 12:35:32 +0100

    Add login component with Arabic placeholders

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <div>
+      <h2>تسجيل الدخول</h2>
+
+      <input placeholder="الإيميل" /><br /><br />
+      <input type="password" placeholder="كلمة المرور" /><br /><br />
+
+      <button>دخول</button>
+    </div>
+  );
+}
