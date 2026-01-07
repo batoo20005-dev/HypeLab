6285a0a779457f320dbde3d7c556a24f3b0914a5
batoo20005-dev <batoo20005@gmail.com>
2026-01-07 12:05:37 +0100

    Add user registration component in Arabic

---
+++ pages/register.js
@@ -0,0 +1,14 @@
+export default function Register() {
+  return (
+    <div>
+      <h2>تسجيل مستخدم</h2>
+
+      <input placeholder="الاسم" /><br /><br />
+      <input placeholder="الإيميل" /><br /><br />
+      <input type="password" placeholder="كلمة المرور" /><br /><br />
+      <input placeholder="البلد" /><br /><br />
+
+      <button>تسجيل</button>
+    </div>
+  );
+}
